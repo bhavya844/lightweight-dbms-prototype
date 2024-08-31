e5cd3af8ad2c14cb7a91b0c6c72536fd9cd2f610
bhavya844 <bhavyadave700@gmail.com>
2024-08-31 12:24:35 -0300

    add: constants for the project

---
+++ src/main/java/org/csci5408/util/Constants.java
@@ -0,0 +1,13 @@
+package org.csci5408.util;
+
+public class Constants {
+    public static String META_DIRECTORY = "meta/";
+    public static String DATABASE_PATH = "./db_path/";
+    public static String DELIMINATOR = ";";
+    public static String EXTENSION = ".txt";
+    public static String DATA_DIRECTORY = "data/";
+    public static String AUTH_FILE = "users" + EXTENSION;
+
+    public static String LOGGER_FILE = "log_info" + EXTENSION;
+    public static String CSV_FILE = "users" + ".csv";
+}
